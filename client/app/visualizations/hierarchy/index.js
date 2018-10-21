e9676dd9068679f37556025703b9ef8680dc0f3d
deecay <daiju.kito@gmail.com>
2018-10-22 05:50:39 +0900

    Initial version

---
+++ client/app/visualizations/hierarchy/index.js
@@ -0,0 +1,275 @@
+/* eslint-disable */
+
+import d3 from 'd3';
+import angular from 'angular';
+import editorTemplate from './hierarchy-editor.html';
+import { partial, map, each, keys, difference, debounce } from 'lodash';
+
+import './hierarchy.less';
+
+function createHierarchy(element, data, scope) {
+  const margin = {top: 20, right: 120, bottom: 20, left: 120};
+  const width = element.clientWidth - margin.right - margin.left;
+  const height = 460 - margin.top - margin.bottom;
+ 
+  let i = 0;
+  const duration = 750;
+  
+  const tree = d3.layout.tree()
+        .size([height, width]);
+  
+  const diagonal = d3.svg.diagonal()
+        .projection(function (d) { return [d.y, d.x]; });
+  
+  const svg = d3.select(element).append('svg')
+        .attr('width', width + margin.right + margin.left)
+        .attr('height', height + margin.top + margin.bottom)
+    .append('g')
+        .attr('transform', `translate(${margin.left},${margin.top})`,);
+
+  const rootTree = data[0];
+  rootTree.x0 = height / 2;
+  rootTree.y0 = 0;
+  // rootTree.children.forEach(collapse);
+
+  function update(source, scope) {
+
+    function collapse(d) {
+      if (d.children) {
+        d._children = d.children;
+        d._children.forEach(collapse);
+        d.children = null;
+      }
+    } 
+  
+    function expand(d){   
+      let children = (d.children)?d.children:d._children;
+      if (d._children) {        
+        d.children = d._children;
+        d._children = null;       
+      }
+      if(children)
+        children.forEach(expand);
+    }
+  
+    function expandAll(){
+      expand(rootTree); 
+      update(rootTree, scope);
+    }
+  
+    // Toggle children on click.
+    function click(d) {
+      const e = event || window.event;
+      if (e.shiftKey) {
+        expandAll();
+      } else {
+        if (d.children) {
+          d._children = d.children;
+          d.children = null;
+        } else {
+          d.children = d._children;
+          d._children = null;
+        }
+        update(d, scope);
+      }
+    }
+  
+    function mouseover(d) {
+      d3.select(this).append('text')
+      .attr('class', 'hover')
+      .attr('transform', function(d){ 
+        return 'translate(5, -10)';
+      })
+      .text(d.Code);
+    }
+  
+    function mouseout(d) {
+      d3.select(this).select('text.hover').remove();
+    }
+  
+    // Compute the new tree layout.
+    const nodes = tree.nodes(rootTree).reverse(),
+          links = tree.links(nodes);
+  
+    // Normalize for fixed-depth.
+    nodes.forEach(function(d) { d.y = d.depth * 400; });
+  
+    // Declare the nodes…
+    const node = svg.selectAll('g.node')
+          .data(nodes, function(d) { return d.id || (d.id = ++i); });
+  
+    // Enter the nodes.
+    const nodeEnter = node.enter().append('g')
+          .attr('class', 'node')
+          .attr('transform', function(d) { 
+                return 'translate(' + source.y0 + ',' + source.x0 + ')'; })
+          .on('click', click)
+          .on('mouseover', mouseover)
+          .on('mouseout', mouseout);
+  
+    nodeEnter.append('circle')
+          .attr('r', 10)
+          .style('fill', function(d) { return d._children ? 'lightsteelblue' : '#fff'; });
+  
+    nodeEnter.append('text')
+          .attr('x', function(d) { 
+                return d.children || d._children ? -13 : 13; })
+          .attr('dy', '.35em')
+          .attr('text-anchor', function(d) { 
+                return d.children || d._children ? 'end' : 'start'; })
+          .text(function(d) { return d[scope.options.childColumn]; })
+          .style('fill-opacity', 1);
+  
+    const nodeUpdate = node.transition()
+      .duration(duration)
+      .attr('transform', function(d) { return 'translate(' + d.y + ',' + d.x + ')'; });
+  
+    nodeUpdate.select('circle')
+      .attr('r', 4.5)
+      .style('fill', function(d) { return d._children ? 'lightsteelblue' : '#fff'; });
+  
+    nodeUpdate.select('text')
+      .style('fill-opacity', 1);
+  
+    // Transition exiting nodes to the parent's new position.
+    const nodeExit = node.exit().transition()
+      .duration(duration)
+      .attr('transform', function(d) { return 'translate(' + source.y + ',' + source.x + ')'; })
+      .remove();
+  
+    nodeExit.select('circle')
+      .attr('r', 1e-6);
+  
+    nodeExit.select('text')
+      .style('fill-opacity', 1e-6);
+  
+    // Declare the links
+    const link = svg.selectAll('path.link')
+          .data(links, function(d) { return d.target.id; });
+  
+    // Enter the links.
+    link.enter().insert('path', 'g')
+      .attr('class', 'link')
+      .attr('d', function(d) {
+        const o = {x: source.x0, y: source.y0};
+        return diagonal({source: o, target: o});
+      });
+  
+    // Transition links to their new position.
+    link.transition()
+      .duration(duration)
+      .attr('d', diagonal);
+  
+    // Transition exiting nodes to the parent's new position.
+    link.exit().transition()
+      .duration(duration)
+      .attr('d', function(d) {
+        const o = {x: source.x, y: source.y};
+        return diagonal({source: o, target: o});
+      })
+      .remove();
+  
+    // Stash the old positions for transition.
+    nodes.forEach(function(d) {
+      d.x0 = d.x;
+      d.y0 = d.y;
+    });
+  }
+  update(rootTree, scope);
+}
+
+function hierarchyRenderer() {
+  return {
+    restrict: 'E',
+    template: '<div class="hierarchy-visualization-container" resize-event="handleResize()"></div>',
+    scope: {
+      queryResult: '=',
+      options: '=',
+    },
+    replace: false,
+    link($scope, element) {
+      $scope.$watch('queryResult && queryResult.getData()', (data) => {
+        if (!data) {
+          return;
+        }
+
+        function refreshData() {
+          const queryData = angular.copy($scope.queryResult.getData());
+
+          // eslint-disable-next-line prefer-arrow-callback
+          const dataMap = queryData.reduce(function (map, node) {
+            map[node[$scope.options.childColumn]] = node;
+            return map;
+          }, {});
+
+          const treeData = [];
+          // eslint-disable-next-line prefer-arrow-callback
+          queryData.forEach(function (node) {
+            // add to parent
+            const parent = dataMap[node[$scope.options.parentColumn]];
+            if (parent) {
+              // create child array if it doesn't exist
+              (parent.children || (parent.children = []))
+                // add node to child array
+                .push(node);
+            } else {
+              // parent is null or missing
+              treeData.push(node);
+            }
+          });
+          if (treeData) {
+            const container = element[0].querySelector('.hierarchy-visualization-container');
+            angular.element(container).empty();
+            createHierarchy(container, treeData, $scope);
+          }
+        }
+        $scope.$watch('queryResult && queryResult.getData()', refreshData);
+        $scope.$watch('options', refreshData, true);
+        $scope.handleResize = debounce(refreshData, 50);
+      });
+
+    },
+  };
+}
+
+function hierarchyEditor() {
+  return {
+    restrict: 'E',
+    template: editorTemplate,
+    scope: {
+      queryResult: '=',
+      options: '=?',
+    },
+    link(scope) {
+      function refreshColumns() {
+        scope.columns = scope.queryResult.getColumns();
+        scope.columnNames = map(scope.columns, i => i.name);
+        if (scope.columnNames.length > 0) {
+          each(difference(keys(scope.options.columnMapping), scope.columnNames), (column) => {
+            delete scope.options.columnMapping[column];
+          });
+        }
+      }
+
+      refreshColumns();
+    }
+  };
+}
+
+export default function init(ngModule) {
+  ngModule.directive('hierarchyRenderer', hierarchyRenderer);
+  ngModule.directive('hierarchyEditor', hierarchyEditor);
+
+  ngModule.config((VisualizationProvider) => {
+    const editTemplate = '<hierarchy-editor options="visualization.options" query-result="queryResult"></hierarchy-editor>';
+
+    VisualizationProvider.registerVisualization({
+      type: 'HIERARCHY',
+      name: 'Hierarchy',
+      renderTemplate: '<hierarchy-renderer resize-event="handleResize()" options="visualization.options" query-result="queryResult"></hierarchy-renderer>',
+      editorTemplate: editTemplate,
+      defaultOptions: { columnMapping: {} },
+    });
+  });
+}
+
---
+++ client/app/visualizations/treemap/index.js
@@ -0,0 +1,179 @@
+/* eslint-disable */
+
+import d3 from 'd3';
+import angular from 'angular';
+import editorTemplate from './treemap-editor.html';
+import { partial, map, each, keys, difference, debounce, pluck, max } from 'lodash';
+import { ColorPalette } from '@/visualizations/chart/plotly/utils';
+
+import './treemap.less';
+
+function createTreemap(element, data, scope) {
+  const margin = {top: 20, right: 50, bottom: 20, left: 50};
+  const width = element.clientWidth - margin.right - margin.left;
+  const height = 460 - margin.top - margin.bottom;
+
+
+  if ((width <= 0) || (height <= 0)) {
+    return;
+  }
+        
+  const svg = d3.select(element).append('svg')
+        .attr('width', width + margin.right + margin.left)
+        .attr('height', height + margin.top + margin.bottom)
+    .append('g')
+        .attr("transform", "translate(-.5,-.5)");
+
+  let tooltip = d3.select("visualization-renderer").append('div').attr("class", "treemap-tooltip");
+
+  const format = d3.format(",d");
+
+  const rootTree = data;
+
+  function update(source) {
+
+    const color = d3.scale.category20c();
+
+    const re = new RegExp('[ \']', 'g');
+
+    d3.select('svg')
+      .attr('height', height + margin.top + margin.bottom)
+      .attr('width', width + margin.left + margin.right);
+
+    let treemap = d3.layout.treemap()
+      .size([width, height])
+      .padding(4)
+      // .sort(function(a, b) { return a.value - b.value; })
+      .value(function(d) { return d.size; });
+
+    const cell = svg.data(source).selectAll("g")
+      .data(treemap.nodes)
+      .enter().append("g")
+      .attr("class", "cell")
+      .attr("transform", function(d) { return "translate(" + margin.left + "," + margin.top + ")"; });
+
+    cell.append("rect")
+      .attr("id", function(d) { return "rect-" + d.name.replace(re, '-'); })
+      .attr("x", function(d) { return d.x; })
+      .attr("y", function(d) { return d.y; })
+      .attr("width", function(d) { return d.dx; })
+      .attr("height", function(d) { return d.dy; })
+      .attr("stroke", "white")
+      .attr("stroke-width", 0.5)
+      .attr("fill", function(d) { return d.children ? color(d.name) : color(d.parent.name); })
+      .on("mousemove", function(d) {
+        tooltip.style("left", d.x + d.dx / 2 + "px");
+        tooltip.style("top", d.y + d.dy / 2 + "px");
+        tooltip.style("display", "block");
+        tooltip.html("<b>" + d.name + "</b>: " + format(d.value));
+      }).on("mouseout", function(d) {
+        tooltip.style("display", "none");
+      });
+
+    cell.append("clipPath")
+      .attr("id", function(d) { return "clip-" + d.name.replace(re, '-'); })
+      .append("use")
+      .attr("xlink:href", function(d) { return "#rect-" + d.name.replace(re, '-') + ""; });
+
+    cell.append("text")
+      .attr("class", "treemap-text")
+      .attr("clip-path", function(d) { return "url(#clip-" + d.name.replace(re, '-') + ")"; })
+      .attr("x", function(d) { return d.x + 3; })
+      .attr("y", function(d) { return d.y; })
+      .attr("dy", "1.3em")
+      .text(function(d) { return d.children ? null : d.name; });
+
+  }
+  update(rootTree);
+}
+
+function treemapRenderer() {
+  return {
+    restrict: 'E',
+    scope: {
+      queryResult: '=',
+      options: '=',
+    },
+    template: '<div class="treemap-visualization-container" resize-event="handleResize()"></div>',
+    replace: false,
+    link($scope, element) {
+
+      function refreshData() {
+        const queryData = angular.copy($scope.queryResult.getData());
+
+        // eslint-disable-next-line prefer-arrow-callback
+        const dataMap = queryData.reduce(function (map, node) {
+          map[node[$scope.options.childColumn]] = node;
+          return map;
+        }, {});
+
+        const treeData = [];
+        // eslint-disable-next-line prefer-arrow-callback
+        queryData.forEach(function (node) {
+          // add to parent
+          const parent = dataMap[node[$scope.options.parentColumn]];
+          if (parent) {
+            // create child array if it doesn't exist
+            (parent.children || (parent.children = []))
+              // add node to child array
+              .push(node);
+          } else {
+            // parent is null or missing
+            treeData.push(node);
+          }
+        });
+        if (treeData) {
+          const container = element[0].querySelector('.treemap-visualization-container');
+          angular.element(container).empty();
+          createTreemap(container, treeData, $scope);
+        }
+      }
+
+      $scope.$watch('queryResult && queryResult.getData()', refreshData);
+      $scope.$watch('options', refreshData, true);
+      $scope.handleResize = debounce(refreshData, 50);
+    },
+  };
+}
+
+function treemapEditor() {
+  return {
+    restrict: 'E',
+    template: editorTemplate,
+    scope: {
+      queryResult: '=',
+      options: '=?',
+    },
+    link(scope) {
+      function refreshColumns() {
+        scope.columns = scope.queryResult.getColumns();
+        scope.columnNames = map(scope.columns, i => i.name);
+        if (scope.columnNames.length > 0) {
+          each(difference(keys(scope.options.columnMapping), scope.columnNames), (column) => {
+            delete scope.options.columnMapping[column];
+          });
+        }
+      }
+
+      // refreshColumns();
+    }
+  };
+}
+
+export default function init(ngModule) {
+  ngModule.directive('treemapRenderer', treemapRenderer);
+  ngModule.directive('treemapEditor', treemapEditor);
+
+  ngModule.config((VisualizationProvider) => {
+    const editTemplate = '<treemap-editor options="visualization.options" query-result="queryResult"></treemap-editor>';
+
+    VisualizationProvider.registerVisualization({
+      type: 'TREEMAP',
+      name: 'Treemap',
+      renderTemplate: '<treemap-renderer options="visualization.options" query-result="queryResult"></treemap-renderer>',
+      editorTemplate: editTemplate,
+      defaultOptions: {},
+    });
+  });
+}
+
